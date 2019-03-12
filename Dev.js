0d5903dfed2c1631a23821db0fc227bf2b5c5255
infernooo <40263169+infernooo@users.noreply.github.com>
2019-03-13 00:07:05 +0530

    Update Dev.js

--- Dev.js
+++ Dev.js
@@ -1,10 +1,41 @@
-const discord = require('discord.js');
+const discord = require("discord.js");
+const http = require('http');
+const token = ''
 
 const bot = new discord.Client();
 
+
 bot.on('ready', () => {
-	console.log('checking!');
+    console.log('Staring')
 })
 
+function spamSt() {
+var spam = setInterval(() =>  {
+    bot.channels.get('551385745743675392').send('spaaaaaaaaaaaaaam');
+}, 2000)
+}
+
+function spamstp(){
+    clearInterval(spam) 
+}
+
+bot.on('message', msg => {
+    if(msg.channel.id === '551385745743675392'){
+        if(msg.channel.includes('.')){
+            spamSt();
+        }
+        else if(msg.channel.includes('A rare pokemon has spawned! Who will catch it?')){
+        spamstp();
+        msg.channel.send(',c 9');
+        console.log('random rare caught')
+        spamSt()
+        }
+    }
+
+})
+
+bot.on('error', err => {
+    console.log('err')
+})
 
 bot.login(process.env.TOKEN);
